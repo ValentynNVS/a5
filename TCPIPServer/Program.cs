0e41c7d905d1f721be66b02948a8ffee8622914b
Ahmed A <163431268+ahmed-moune@users.noreply.github.com>
2024-11-11 17:37:22 -0500

    Added client project

--- TCPIPServer/Program.cs
+++ TCPIPServer/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace a5
+namespace TCPIPServer
 {
-    internal class test
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
