5d18b775ec298356408e0c12947874141ff2313c
Valentyn Novosydliuk <Vnovosydliuk0335@conestogac.on.ca>
2024-11-18 23:03:46 -0500

    changed end clikc

--- TCPIPClient/MainWindow.xaml.cs
+++ TCPIPClient/MainWindow.xaml.cs
@@ -217,61 +217,68 @@ namespace TCPIPClient
 
         private void EndGameButton_Click(object sender, RoutedEventArgs e)
         {
-                finishGameQuestion();
+            if (finishGameQuestion() == false)
+            {
+                return;
+            }
+            else
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 0;
+                Int32.TryParse(PortTextBox.Text, out port);
+                String server = IpAddressTextBox.Text;
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                String message = "EndPlayerSession|" + sessionId;
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                TargetWordTextBlock.Text = responseData;
+                Console.WriteLine("Received: {0}", responseData);
+
+                IpAddressTextBox.IsEnabled = true;
+                PortTextBox.IsEnabled = true;
+                NameTextBox.IsEnabled = true;
+                TimeLimitTextBox.IsEnabled = true;
+                ConnectButton.IsEnabled = true;
+                ResultTextBlock.Text = "The game is stopped";
+                ResultTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+                StatusTextBlock.Text = "Status: Disconnected";
+                StatusTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+                gameTimer.Stop();
+                // Close everything.
+                stream.Close();
+                client.Close();
+
+                connected = false;
+            }    
 
 
-            // Create a TcpClient.
-            // Note, for this client to work you need to have a TcpServer 
-            // connected to the same address as specified by the server, port
-            // combination.
-            Int32 port = 0;
-            Int32.TryParse(PortTextBox.Text, out port);
-            String server = IpAddressTextBox.Text;
-            TcpClient client = new TcpClient(server, port);
-
-            // Translate the passed message into ASCII and store it as a Byte array.
-            String message = "EndPlayerSession|" + sessionId;
-            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-            // Get a client stream for reading and writing.
-            //  Stream stream = client.GetStream();
-
-            NetworkStream stream = client.GetStream();
-
-            // Send the message to the connected TcpServer. 
-            stream.Write(data, 0, data.Length);
-
-            Console.WriteLine("Sent: {0}", message);
-
-            // Receive the TcpServer.response.
-
-            // Buffer to store the response bytes.
-            data = new Byte[256];
-
-            // String to store the response ASCII representation.
-            String responseData = String.Empty;
-
-            // Read the first batch of the TcpServer response bytes.
-            Int32 bytes = stream.Read(data, 0, data.Length);
-            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-            TargetWordTextBlock.Text = responseData;
-            Console.WriteLine("Received: {0}", responseData);
-
-            IpAddressTextBox.IsEnabled = true;
-            PortTextBox.IsEnabled = true;
-            NameTextBox.IsEnabled = true;
-            TimeLimitTextBox.IsEnabled = true;
-            ConnectButton.IsEnabled = true;
-            ResultTextBlock.Text = "The game is stopped";
-            ResultTextBlock.Foreground = new SolidColorBrush(Colors.Red);
-            StatusTextBlock.Text = "Status: Disconnected";
-            StatusTextBlock.Foreground =   new SolidColorBrush(Colors.Red);
-            gameTimer.Stop();
-            // Close everything.
-            stream.Close();
-            client.Close();
-
-            connected = false;
+          
 
         }
 
@@ -310,18 +317,19 @@ namespace TCPIPClient
                 EndGameButton_Click(this, new RoutedEventArgs());
             }
         }
-        private void finishGameQuestion()
+        private bool finishGameQuestion()
         {
             if (!connected)
             {
                 MessageBox.Show("You are not connected to a server.", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
+                return true;
             }
             MessageBoxResult mbResult = MessageBox.Show("Are you sure you want to exit.", "Connection Error", MessageBoxButton.YesNo);
             if (mbResult == MessageBoxResult.No)
             {
-                return;
+                return false;
             }
+            return true;
         }
         private void TimeLimitTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
