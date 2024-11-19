7f09b3dc742adbee3175af559248ed9f2d49887d
Ahmed A <163431268+ahmed-moune@users.noreply.github.com>
2024-11-18 20:09:13 -0500

    Ability to end game

--- TCPIPClient/MainWindow.xaml.cs
+++ TCPIPClient/MainWindow.xaml.cs
@@ -205,6 +205,46 @@ namespace TCPIPClient
                 return;
             }
 
+            // Create a TcpClient.
+            // Note, for this client to work you need to have a TcpServer 
+            // connected to the same address as specified by the server, port
+            // combination.
+            Int32 port = 0;
+            Int32.TryParse(PortTextBox.Text, out port);
+            String server = IpAddressTextBox.Text;
+            TcpClient client = new TcpClient(server, port);
+
+            // Translate the passed message into ASCII and store it as a Byte array.
+            String message = "EndPlayerSession|" + sessionId;
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+            // Get a client stream for reading and writing.
+            //  Stream stream = client.GetStream();
+
+            NetworkStream stream = client.GetStream();
+
+            // Send the message to the connected TcpServer. 
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Sent: {0}", message);
+
+            // Receive the TcpServer.response.
+
+            // Buffer to store the response bytes.
+            data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            TargetWordTextBlock.Text = responseData;
+            Console.WriteLine("Received: {0}", responseData);
+
+            // Close everything.
+            stream.Close();
+            client.Close();
 
             connected = false;
 
