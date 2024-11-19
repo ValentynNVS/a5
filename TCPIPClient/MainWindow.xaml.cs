b5a75cf53e536b193c9bf2f9fa3515307ee9417c
Ahmed A <163431268+ahmed-moune@users.noreply.github.com>
2024-11-18 22:02:24 -0500

    idk

--- TCPIPClient/MainWindow.xaml.cs
+++ TCPIPClient/MainWindow.xaml.cs
@@ -211,15 +211,22 @@ namespace TCPIPClient
             {
                 gameTimer.Stop();
                 TimerTextBlock.Text = "Time's up!";
-                ShowEndGameDialog();
+                EndGameButton_Click(this, new RoutedEventArgs());
             }
         }
 
         private void EndGameButton_Click(object sender, RoutedEventArgs e)
         {
-                finishGameQuestion();
-
-
+            if (!connected)
+            {
+                MessageBox.Show("You are not connected to a server.", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            MessageBoxResult mbResult = MessageBox.Show("Are you sure you want to exit.", "Connection Error", MessageBoxButton.YesNo);
+            if (mbResult == MessageBoxResult.No)
+            {
+                return;
+            }
             // Create a TcpClient.
             // Note, for this client to work you need to have a TcpServer 
             // connected to the same address as specified by the server, port
@@ -266,7 +273,6 @@ namespace TCPIPClient
             ResultTextBlock.Foreground = new SolidColorBrush(Colors.Red);
             StatusTextBlock.Text = "Status: Disconnected";
             StatusTextBlock.Foreground =   new SolidColorBrush(Colors.Red);
-            gameTimer.Stop();
             // Close everything.
             stream.Close();
             client.Close();
@@ -275,54 +281,6 @@ namespace TCPIPClient
 
         }
 
-        private void ResetGame()
-        {
-            IpAddressTextBox.IsEnabled = true;
-            PortTextBox.IsEnabled = true;
-            NameTextBox.IsEnabled = true;
-            TimeLimitTextBox.IsEnabled = true;
-            ConnectButton.IsEnabled = true;
-            ResultTextBlock.Text = "The game is stopped";
-            ResultTextBlock.Foreground = new SolidColorBrush(Colors.Red);
-            StatusTextBlock.Text = "Status: Disconnected";
-            StatusTextBlock.Foreground = new SolidColorBrush(Colors.Red);
-            gameTimer.Stop();
-            // Close everything.
-
-            connected = false;
-
-        }
-
-        private void ShowEndGameDialog()
-        {
-            // Show a dialog box when the game ends
-            MessageBoxResult result = MessageBox.Show("Congratulations! You've finished the game. Would you like to play again?", "Game Over", MessageBoxButton.YesNo, MessageBoxImage.Information);
-
-            if (result == MessageBoxResult.Yes)
-            {
-                // Reset the game for a new round
-                EndGameButton_Click(this, new RoutedEventArgs());
-                ResetGame();
-            }
-            else
-            {
-                // Stop the game and disconnect
-                EndGameButton_Click(this, new RoutedEventArgs());
-            }
-        }
-        private void finishGameQuestion()
-        {
-            if (!connected)
-            {
-                MessageBox.Show("You are not connected to a server.", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
-            MessageBoxResult mbResult = MessageBox.Show("Are you sure you want to exit.", "Connection Error", MessageBoxButton.YesNo);
-            if (mbResult == MessageBoxResult.No)
-            {
-                return;
-            }
-        }
         private void TimeLimitTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
